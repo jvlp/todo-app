e85d38421290e0c281bac217395a95f1c169f3ab
João Peixoto <jvlp@cin.ufpe.br>
2023-06-23 23:29:07 -0300

    implements infinity scroll

--- frontend/src/Pages/TodoList.jsx
+++ frontend/src/Pages/TodoList.jsx
@@ -1,22 +1,48 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import TodoItem from "../Components/TodoItem";
 import { AiOutlineArrowUp } from "react-icons/ai";
 import axios from "axios";
 
 function TodoList() {
-  const [tasks, setTasks] = useState([]);
+  const pageSize = 10;
 
-  // fetch tasks when mounted
+  const [tasks, setTasks] = useState([]);
+  const [pageNumber, setPageNumber] = useState(0);
+  const [loading, setLoading] = useState(true);
+  const [hasMore, setHasMore] = useState(false);
+
+  // fetch tasks
   useEffect(() => {
+    setLoading(true);
     axios
-      .get("http://127.0.0.1:3000/api/v1/tasks")
+      .get(
+        `http://127.0.0.1:3000/api/v1/tasks?pageSize=${pageSize}&pageNumber=${pageNumber}`
+      )
       .then((response) => {
-        setTasks(response.data);
+        setTasks((current) => [...current, ...response.data]);
+        setHasMore(response.data.length > 0);
+        setLoading(false);
       })
       .catch((error) => {
         console.log(error);
       });
-  }, []);
+  }, [pageNumber]);
+
+  // observe last task and add 1 to pageNumber when it is visible
+  const observer = useRef();
+  const lastTaskRef = useCallback(
+    (task) => {
+      if (loading) return;
+      if (observer.current) observer.current.disconnect();
+      observer.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting && hasMore) {
+          setPageNumber((current) => current + 1);
+        }
+      });
+      if (task) observer.current.observe(task);
+    },
+    [loading, hasMore]
+  );
 
   function handleDeleteTask(id) {
     console.log(`http://127.0.0.1:3000/api/v1/tasks/${id}`);
@@ -47,31 +73,45 @@ function TodoList() {
       });
   }
 
-  const shouldShowArrowUp = () => {
-    return document.documentElement.scrollTop > window.innerHeight;
-  };
-
   return (
     <>
       <div className="flex h-fit w-[80%] flex-col rounded-md bg-slate-200 p-6">
         <h1 className="mb-8 self-center text-4xl font-extrabold text-sky-800">
           Lista de Tarefas
         </h1>
-        {tasks.map((task) => (
-          <TodoItem
-            key={task.id}
-            task={task}
-            handleDeleteTask={handleDeleteTask}
-            handleFinishTask={handleFinishTask}
-          />
-        ))}
+        {tasks.map((task, index) => {
+          // add reference to last task
+          if (index + 1 === tasks.length) {
+            return (
+              <div ref={lastTaskRef} key={task.id}>
+                <TodoItem
+                  task={task}
+                  handleDeleteTask={handleDeleteTask}
+                  handleFinishTask={handleFinishTask}
+                />
+              </div>
+            );
+          } else {
+            return (
+              <TodoItem
+                key={task.id}
+                task={task}
+                handleDeleteTask={handleDeleteTask}
+                handleFinishTask={handleFinishTask}
+              />
+            );
+          }
+        })}
+        <div className="mb-8 self-center text-xl font-extrabold text-sky-800">
+          {loading && "Loading..."}
+        </div>
       </div>
-      {shouldShowArrowUp() && (
+      {pageNumber > 1 && (
         <button
           onClick={() => {
             document.documentElement.scrollTop = 0;
           }}
-          className="fixed bottom-10 right-10 flex  h-10 w-10 items-center justify-center rounded-lg bg-sky-700"
+          className="fixed bottom-10 right-10 flex h-10 w-10 items-center justify-center rounded-lg bg-sky-700"
         >
           <AiOutlineArrowUp />
         </button>
